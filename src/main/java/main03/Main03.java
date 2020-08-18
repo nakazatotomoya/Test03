d3d055038b34d82a4ccfe0b0a05abbda0f7f5b15
tomoya nakazato <t.nakazato@good-works.co.jp>
2020-08-18 18:05:10 +0900

    add

---
+++ src/main/java/main03/Main03.java
@@ -0,0 +1,16 @@
+package main03;
+
+public class Main03 {
+
+	public String getColor(int i) {
+
+        if (i == 1) {
+            return "赤";
+        } else if (i == 2) {
+            return "青";
+
+        } else {
+            return "1or2を入力してください";
+        }
+    }
+}
