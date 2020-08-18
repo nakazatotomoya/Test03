0ee658e2e47ebeda45538688c00411b9a7c95e28
tomoya nakazato <t.nakazato@good-works.co.jp>
2020-08-18 18:18:08 +0900

    add

--- src/test/java/test03/Test03.java
+++ src/test/java/test03/Test03.java
@@ -1,5 +1,30 @@
 package test03;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+import main03.Main03;
+
 public class Test03 {
 
+	@Test
+    public void testGetColor1() {
+		Main03 c1 = new Main03();
+        String t1 = c1.getColor(1);
+        assertThat(t1,is("赤"));
+    }
+    @Test
+    public void testGetColor2() {
+    	Main03 c1 = new Main03();
+        String t1 = c1.getColor(2);
+        assertThat(t1,is("青"));
+    }
+    @Test
+    public void testGetColor3() {
+    	Main03 c1 = new Main03();
+        String t1 = c1.getColor(3);
+        assertThat(t1,is("1or2を入力してください"));
+    }
 }
